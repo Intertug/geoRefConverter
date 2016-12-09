9b736d0e291d8f2396d7ddf484f278b0ae5dda15
Rubén Melo <rbmcar2@gmail.com>
2016-12-09 17:48:20 -0500

    CLI options

--- changeCoordinates.py
+++ changeCoordinates.py
@@ -45,3 +45,23 @@ def createDecimal(coordinates):
 
 	return (round(north, 2), round(south, 2))
 
+question = raw_input("""
+Which coordinate to convert: 
+(1) With NW to Decimal
+(2) With Decimal to NW
+>>> """)
+
+if int(question) == 1:
+	coordinates = raw_input("Enter Coordinates in format 1043.2 N 7532.4 W: ")
+	coordinates = coordinates.split(" ")
+	coorTuple = (float(coordinates[0]), coordinates[1], float(coordinates[2]), coordinates[3])
+	answer = createDecimal(coorTuple)
+	print "The coordinate is:", answer
+elif int(question) == 2:
+	coordinates = raw_input("Enter Coordinates in format 10.432,-75.32: ")
+	coordinates = coordinates.split(",")
+	coorTuple = (float(coordinates[0]), float(coordinates[1]))
+	answer = createNW(coorTuple)
+	print "The coordinate is:", answer	
+else:
+	print "Wrong choice"
